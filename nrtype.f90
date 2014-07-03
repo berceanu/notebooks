2f32bcadf4c268b383a90ad2a94a37aee3049c49
Andrei Berceanu <berceanu@runbox.com>
2014-07-03 19:36:24 +0200

    analytically implemented gaussian defect for 1 pump only

--- nrtype.f90
+++
@@ -1,7 +0,0 @@
-module nrtype
-  integer, parameter:: dp=kind(0.d0)             ! double
-  integer, parameter :: dpc = kind((0.d0, 0.d0)) ! double complex
-  complex(dpc), parameter :: one = (1.d0, 0.d0)  ! complex 1
-  complex(dpc), parameter :: I = (0.d0, 1.d0)    ! complex i
-  complex(dpc), parameter :: zero = (0.d0, 0.d0) ! complex 0
-end module nrtype
