69ee05f628ea2948066f7a06f5348537dec35e39
Andrei Berceanu <berceanu@runbox.com>
2014-07-13 11:37:10 +0200

    expressed k-space modulation in terms of dispersion

--- julia/OnePumpQuad.jl
+++ julia/OnePumpQuad.jl
@@ -8,6 +8,6 @@ quadλ1(ky::Float64, kx::Float64; vp=(0., 1.9), Δp=0., κ=1.1) = (ky*vp[1] + kx
 
 quadλ2(ky::Float64, kx::Float64; vp=(0., 1.9), Δp=0., κ=1.1) = (ky*vp[1] + kx*vp[2]) -im*κ - sqrt(ε(ky, kx; Δp=Δp)*(ε(ky, kx; Δp=Δp) + 2))
 
-quadψtmom(ky::Float64, kx::Float64; gv=1e-2, vp=(0., 1.9), Δp=0., κ=1.1) = -gv*(ε(ky, kx; Δp=Δp) - (ky*vp[1] + kx*vp[2]) + im*κ)/(ε(ky, kx; Δp=Δp)*(ε(ky, kx; Δp=Δp) + 2)- (ky*vp[1] + kx*vp[2] - im*κ)^2)
+quadψtmom(ky::Float64, kx::Float64; gv=1e-2, vp=(0., 1.9), Δp=0., κ=1.1) = gv*(ε(ky, kx; Δp=Δp) - (ky*vp[1] + kx*vp[2]) + im*κ)/(quadλ1(ky, kx; Δp=Δp, vp=vp, κ=κ)*quadλ2(ky, kx; Δp=Δp, vp=vp, κ=κ))
 
 end
