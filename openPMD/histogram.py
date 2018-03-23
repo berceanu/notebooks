a117cea7297639942eb008876972494bb111b2c3
berceanu <andrei.berceanu@eli-np.ro>
2018-03-23 03:50:02 +0200

    pep8 compliant

--- openPMD/histogram.py
+++ openPMD/histogram.py
@@ -1,44 +1,21 @@
 """This module reimplements the ``EnergyHistogram`` function from ``Auswertung``."""
 
-import os, h5py
+import os
+import h5py
 import scipy.constants as const
 import numpy as np
 import logging
-logging.basicConfig(level=logging.DEBUG)
 
-#import dask.array as da
-#x = da.from_array(dset, chunks=(1000, 1000))
-
-#import pdb
-#pdb.set_trace()
-
-#TODO: move to variouz.py
-#def chunk_range(x1, x2, numb):
-#    size = ((x2 - x1 + 1) // numb) + 1
-#    while x1 <= x2:
-#        next = x1 + size
-#        yield x1, min(next - 1, x2)
-#        x1 = next
-
-#def chunk_range(x1, x2, size):
-#    while x1 <= x2:
-#        next = x1 + size
-#        yield x1, min(next - 1, x2)
-#        x1 = next
 
 def chunk_range(x1, x2, size):
     while x1 <= x2:
-        next = x1 + size
-        yield x1, min(next, x2)
-        x1 = next
+        nxt = x1 + size
+        yield x1, min(nxt, x2)
+        x1 = nxt
+
 
 def energy_histogram(timestep=50000, root='.', prefix='h5_all', slice_size=2**20,
-                     species='e', n_bins=1024, bin_min=20, bin_max=150, mask='all',
-                     logfile=None):
-
-    if logfile: #turn on logging to file
-        logging.basicConfig(filename=logfile)
-
+                     species='e', n_bins=1024, bin_min=20, bin_max=150, mask='all'):
 
     # build full path to file
     filename = os.path.join(root, 'h5/{}_{}.h5'.format(prefix, timestep))
@@ -47,62 +24,51 @@ def energy_histogram(timestep=50000, root='.', prefix='h5_all', slice_size=2**20
     f = h5py.File(filename, 'r')
     logging.info('done')
 
-
     h5_key = '/data/{}/particles/{}'.format(timestep, species)
     particle_group = f[h5_key]
     weights_dset = particle_group['weighting']
-    px_dset = particle_group['momentum/x']
-    py_dset = particle_group['momentum/y']
-    pz_dset = particle_group['momentum/z']
-    N_all = weights_dset.size
-
+    mom_dset = particle_group['momentum']
+    n_datapoints = weights_dset.size
 
     if mask == 'random':
         # seed the random number generator for reproducibility
         np.random.seed(42)
-        mask = np.random.choice([True, False], N_all)
+        mask = np.random.choice([True, False], n_datapoints)
     elif mask == 'all':
-        mask = np.full(N_all, True)
+        mask = np.full(n_datapoints, True)
     else:
         raise ValueError('mask must be either \'all\' or \'random\'.')
 
-
-    n_trues = np.sum(mask) # nr. of True values
-    E = np.zeros(n_trues, dtype=np.float32)
+    n_trues = np.sum(mask)  # nr. of True values
+    energy = np.zeros(n_trues, dtype=np.float32)
     weights = np.zeros(n_trues, dtype=np.float32)
 
-
-    convert = px_dset.attrs['unitSI'] / const.speed_of_light / const.electron_mass
+    ratio = mom_dset['x'].attrs['unitSI'] / const.speed_of_light / const.electron_mass
 
     a, b = 0, 0
 
-    for st, end in chunk_range(0, N_all, slice_size):
-        logfile.info('processing chuck from {} to {}'.format(st, end))
+    for st, end in chunk_range(0, n_datapoints, slice_size):
+        logging.info('processing chunk from {} to {} of {}'.format(st, end, n_datapoints))
 
-        px, py, pz = px_dset[st:end], py_dset[st:end], pz_dset[st:end]
+        logging.info('slicing datasets')
+        px, py, pz = mom_dset['x'][st:end], mom_dset['y'][st:end], mom_dset['z'][st:end]
         w = weights_dset[st:end]
         m = mask[st:end]
+        logging.info('done')
 
         a, b = b, b + np.sum(m)
+        logging.info('a = {}, b = {}'.format(a, b))
 
-        logfile.info('a = {}, b = {}'.format(a, b))
+        usq = (px[m]**2 + py[m]**2 + pz[m]**2) / w[m]**2
+        gamma = np.sqrt(1 + usq * ratio**2)
 
+        energy[a:b] = (gamma - 1) * 0.511
         weights[a:b] = w[m]
-        norm = weights[a:b]
-
-        pxsq = px[m]**2
-        pysq = py[m]**2
-        pzsq = pz[m]**2
-
-        usq = (pxsq + pysq + pzsq) * convert**2 / norm**2
-        gamma = np.sqrt(1 + usq)
-        E[a:b] = (gamma - 1) * 0.511
 
     f.close()
 
-    hist, _ = np.histogram(E, bins=n_bins, range=(bin_min, bin_max), weights=weights)
+    hist, _ = np.histogram(energy, bins=n_bins, range=(bin_min, bin_max), weights=weights)
     return hist
-
 
 
 if __name__ == '__main__':
--- openPMD/prof_histogram.py
+++ openPMD/prof_histogram.py
@@ -1,8 +1,10 @@
-import histogram as h
 import cProfile
+import logging
+import histogram as h
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, filename='debug_histogram.log')
     prof = cProfile.Profile()
-    prof.runcall(h.energy_histogram, timestep=5000, logfile='log_histogram.log')
+    prof.runcall(h.energy_histogram, timestep=5000)
     prof.dump_stats('stats.prof')
